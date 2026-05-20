74159a32346563ccecfd81bc9b1b0b1adbf15437
ruthesieboma42 <ruthesieboma@gmail.com>
2026-05-20 09:57:18 +0100

    removed database credentials

--- Shiply.API/Program.cs
+++ Shiply.API/Program.cs
@@ -6,6 +6,11 @@ using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(
+        builder.Configuration.GetConnectionString("DefaultConnection")
+    ));
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
@@ -73,7 +78,12 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+
+       
+    });
 }
 
 app.UseHttpsRedirection();
